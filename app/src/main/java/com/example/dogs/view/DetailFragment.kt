812af5a2a741f8d4819daa6a3694b7929b2b3df9
prashantkumarrai <prashant.rai@gonuclei.com>
2019-09-04 00:30:52 +0530

    navigation

--- app/src/main/java/com/example/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogs/view/DetailFragment.kt
@@ -8,14 +8,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dogs.R
 
-class ListFragment : Fragment() {
+class DetailFragment : Fragment() {
 
+    private var dogUuid = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+        }
     }
 }
